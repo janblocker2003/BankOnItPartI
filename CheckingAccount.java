01658f7d37dd13bc9e8a8fac5c54ae692fddde62
janblocker2003 <janblocker2003@gmail.com>
2026-03-20 20:09:20 +0000

    made menu and makefile and start

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -11,7 +11,7 @@ public class CheckingAccount implements HasMenu, Serializable {
 		ca.start();
 	} // end main (tester function)
 	
-	public CheckingAccount()[
+	public CheckingAccount(){
 		this.balance = 0d; //d means this is a double
 	} // end no parameter constructor
 	
@@ -21,6 +21,7 @@ public class CheckingAccount implements HasMenu, Serializable {
 	
 	public String menu(){
 		Scanner input = new Scanner(System.in);
+		System.out.println();
 		System.out.println("0) Quit");
 		System.out.println("1) Check Balance");
 		System.out.println("2) Make a Deposit");
@@ -32,19 +33,26 @@ public class CheckingAccount implements HasMenu, Serializable {
 	} // end menu
 	
 	public void start(){
-		boolean keepGoing = false;
+		boolean keepGoing = true;
 		String response;
 		while (keepGoing) {
 			response = menu();
 			if (response.equals("0")){
-				keepgoing = false;
+				keepGoing = false;
 			} // end if
 			else if (response.equals("1")){
-				System.out.println("Checking balance now... ");
+				System.out.println(" Checking balance now... ");
 			} // end else if 1
 			else if (response.equals("2")){
-
-
+				System.out.println(" Making a desposit now...");
+			} // end else if 2
+			else if (response.equals("3")){
+				System.out.println(" Making a withdrawal now... ");
+			} // end else if 3
+			else {
+				System.out.println( " Please enter 0, 1, 2, or 3 ");
+			} // end else
+		} // end while loop
 	} // end start
 
 } // end Checking Account Class definition
