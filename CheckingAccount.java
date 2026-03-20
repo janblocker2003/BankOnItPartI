226b1398a9bcdb81cebb59647c01c5108b5e8225
janblocker2003 <janblocker2003@gmail.com>
2026-03-20 19:25:54 +0000

    computer glitching, need to save and reset computer

---
+++ CheckingAccount.java
@@ -0,0 +1,50 @@
+//Checking ACcount code
+
+import java.util.*;
+import java.io.*; //using second week
+
+public class CheckingAccount implements HasMenu, Serializable {
+	double balance;
+
+	public static void main(String[] args) {
+		CheckingAccount ca = new CheckingAccount();
+		ca.start();
+	} // end main (tester function)
+	
+	public CheckingAccount()[
+		this.balance = 0d; //d means this is a double
+	} // end no parameter constructor
+	
+	public CheckingAccount(double balance) {
+		this.balance = balance;
+	} // end constructor with parameter
+	
+	public String menu(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("0) Quit");
+		System.out.println("1) Check Balance");
+		System.out.println("2) Make a Deposit");
+		System.out.println("3) Make a Withdrawal");
+		System.out.println();
+		System.out.print("Enter 0-3: ");
+		String response = input.nextLine();
+		return response;
+	} // end menu
+	
+	public void start(){
+		boolean keepGoing = false;
+		String response;
+		while (keepGoing) {
+			response = menu();
+			if (response.equals("0")){
+				keepgoing = false;
+			} // end if
+			else if (response.equals("1")){
+				System.out.println("Checking balance now... ");
+			} // end else if 1
+			else if (response.equals("2")){
+
+
+	} // end start
+
+} // end Checking Account Class definition
